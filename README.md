3e6e289b94b98c4763efeb9b4dae2283e804018e
xaoxuu <me@xaoxuu.com>
2021-06-24 11:39:27 +0800

    自动记住端口号

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 export http_proxy config
 
 ```
-export http_proxy="http://127.0.0.1: $P1"
-export HTTP_PROXY="http://127.0.0.1: $P1"
-export https_proxy="http://127.0.0.1: $P1"
-export HTTPS_PROXY="http://127.0.0.1: $P1"
+export http_proxy="http://127.0.0.1:$P1"
+export HTTP_PROXY="http://127.0.0.1:$P1"
+export https_proxy="http://127.0.0.1:$P1"
+export HTTPS_PROXY="http://127.0.0.1:$P1"
 ```
 
 ## Install
@@ -24,8 +24,8 @@ proxy 58884
 equal to:
 
 ```
-export http_proxy="http://127.0.0.1: 58884"
-export HTTP_PROXY="http://127.0.0.1: 58884"
-export https_proxy="http://127.0.0.1: 58884"
-export HTTPS_PROXY="http://127.0.0.1: 58884"
+export http_proxy="http://127.0.0.1:58884"
+export HTTP_PROXY="http://127.0.0.1:58884"
+export https_proxy="http://127.0.0.1:58884"
+export HTTPS_PROXY="http://127.0.0.1:58884"
 ```
--- proxy.sh
+++ proxy.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
+# 设置新的端口号
 P1=$1
 
-function copy_code() {
-  export http_proxy="http://127.0.0.1: $P1"
-  export HTTP_PROXY="http://127.0.0.1: $P1"
-  export https_proxy="http://127.0.0.1: $P1"
-  export HTTPS_PROXY="http://127.0.0.1: $P1"
+# 记住端口号，下次无需再次输入
+PORT=""
+
+function export_code() {
+  export http_proxy="http://127.0.0.1:$PORT"
+  export HTTP_PROXY="http://127.0.0.1:$PORT"
+  export https_proxy="http://127.0.0.1:$PORT"
+  export HTTPS_PROXY="http://127.0.0.1:$PORT"
 }
 
-function start() {
-	case $P1 in
+function set_port() {
+  case $P1 in
     '')
-      echo '> 格式为：「proxy 端口号」'
+      echo '> 设置端口号格式为：「proxy 端口号」' 
       ;;
     *)
-      copy_code && echo '> Success!'
+      sudo sed -i "" "s/^PORT=\([^,]\{1,\}\)/PORT=\"$P1\"/g" '/usr/local/bin/proxy' &&
+      PORT=$P1 && P1='' && start
       ;;
   esac
 }
 
-start $1
+function start() {
+  case $P1 in
+    '')
+      case $PORT in
+        '')
+          set_port ;;
+        *)
+          export_code && printf "\n> Proxy: \033[32m%s$PORT\033[0m\n\n" 'http://127.0.0.1:' ;;
+      esac
+      ;;
+    *)
+      set_port ;;
+  esac
+}
+
+start
