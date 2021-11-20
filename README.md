c50e8cb0f314e1c6a627fc06e3b99f7d6e975074
xaoxuu <git@xaoxuu.com>
2021-11-20 16:08:19 +0800

    update

--- README.md
+++ README.md
@@ -2,13 +2,6 @@
 
 export http_proxy config
 
-```
-export http_proxy="http://127.0.0.1:$P1"
-export HTTP_PROXY="http://127.0.0.1:$P1"
-export https_proxy="http://127.0.0.1:$P1"
-export HTTPS_PROXY="http://127.0.0.1:$P1"
-```
-
 ## Install
 
 ```
--- proxy.sh
+++ proxy.sh
@@ -4,23 +4,26 @@
 P1=$1
 
 # 记住端口号，下次无需再次输入
-PORT=""
+PROXY_PORT=""
 
 function export_code() {
-  export http_proxy="http://127.0.0.1:$PORT"
-  export HTTP_PROXY="http://127.0.0.1:$PORT"
-  export https_proxy="http://127.0.0.1:$PORT"
-  export HTTPS_PROXY="http://127.0.0.1:$PORT"
+  PROXY=http://127.0.0.1:$PROXY_PORT
+  ALL_PROXY="export http_proxy=\"$PROXY\"; export HTTP_PROXY=\"$PROXY\"; export https_proxy=\"$PROXY\"; export HTTPS_PROXY=\"$PROXY\""
+  echo $ALL_PROXY | pbcopy
+}
+
+function echo_proxy() {
+  echo `pbpaste`
+  echo '代码已复制到剪切板！'
 }
 
 function set_port() {
   case $P1 in
-    '')
-      echo '> 设置端口号格式为：「proxy 端口号」' 
-      ;;
+    '') echo '> 设置端口号格式为：「proxy 端口号」' ;;
+    'echo') echo_proxy ;;
     *)
-      sudo sed -i "" "s/^PORT=\([^,]\{1,\}\)/PORT=\"$P1\"/g" '/usr/local/bin/proxy' &&
-      PORT=$P1 && P1='' && start
+      sudo sed -i "" "s/^PROXY_PORT=\([^,]\{1,\}\)/PROXY_PORT=\"$P1\"/g" '/usr/local/bin/proxy' &&
+      PROXY_PORT=$P1 && P1='' && start
       ;;
   esac
 }
@@ -28,11 +31,11 @@ function set_port() {
 function start() {
   case $P1 in
     '')
-      case $PORT in
+      case $PROXY_PORT in
         '')
           set_port ;;
         *)
-          export_code && printf "\n> Proxy: \033[32m%s$PORT\033[0m\n\n" 'http://127.0.0.1:' ;;
+          export_code && echo_proxy ;;
       esac
       ;;
     *)
